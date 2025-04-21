0e7c449bc21bcb679c1fed9db4f420758d2dbac6
ITxiao666 <2378834248@qq.com>
2025-04-21 22:05:24 +0800

    initial commit

---
+++ xposed/build.gradle.kts
@@ -0,0 +1,32 @@
+import com.android.ide.common.signing.KeystoreHelper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.PrintStream
+import java.util.Locale
+
+plugins {
+    alias(libs.plugins.agp.lib)
+    alias(libs.plugins.refine)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.android.hmal.xposed"
+
+    buildFeatures {
+        buildConfig = false
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(projects.common)
+
+    implementation(libs.com.android.tools.build.apksig)
+    implementation(libs.com.github.kyuubiran.ezxhelper)
+    implementation(libs.dev.rikka.hidden.compat)
+    compileOnly(libs.de.robv.android.xposed.api)
+    compileOnly(libs.dev.rikka.hidden.stub)
+}
